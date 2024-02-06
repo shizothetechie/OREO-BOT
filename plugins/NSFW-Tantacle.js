c77b0046f916a6d763efe061a53fd6c0e2918802
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-02-06 10:58:57 +0530

    Update NSFW-Tantacle.js

--- plugins/NSFW-Tantacle.js
+++ plugins/NSFW-Tantacle.js
@@ -7,7 +7,7 @@ let endpoint = `https://shizoapi.onrender.com/api/nsfw/tantacle?apikey=${shizoke
 const response = await fetch(endpoint);
 if (response.ok) {
       const imageBuffer = await response.buffer();
-      await conn.sendFile(m.chat, imageBuffer, 'shizo.techie.error.png', msg, m);
+      await conn.sendFile(m.chat, imageBuffer, 'shizo.techie.error.png', msg, m, null, rpig);
     } else {
       throw bug
     }
