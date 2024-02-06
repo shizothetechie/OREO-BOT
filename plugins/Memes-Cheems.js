3d5e2bc3964ddfcdf5d7b973f29a7bcc7e5dc75b
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-02-06 10:44:42 +0530

    Update Memes-Cheems.js

--- plugins/Memes-Cheems.js
+++ plugins/Memes-Cheems.js
@@ -7,7 +7,7 @@ let endpoint = `https://shizoapi.onrender.com/api/memes/cheems?apikey=${shizokey
 const response = await fetch(endpoint);
 if (response.ok) {
       const imageBuffer = await response.buffer();
-      await conn.sendFile(m.chat, imageBuffer, 'shizo.techie.error.png', msg, m);
+      await conn.sendFile(m.chat, imageBuffer, 'shizo.techie.error.png', msg, m, null, rpyt);
     } else {
       throw bug
     }
