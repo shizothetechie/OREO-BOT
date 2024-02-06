0c92c357d28b56aa7e9eb303b1ff6cd9458bb96e
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-02-06 10:51:33 +0530

    Update NSFW-GangBang.js

--- plugins/NSFW-GangBang.js
+++ plugins/NSFW-GangBang.js
@@ -7,7 +7,7 @@ let endpoint = `https://shizoapi.onrender.com/api/nsfw/gangbang?apikey=${shizoke
 const response = await fetch(endpoint);
 if (response.ok) {
       const imageBuffer = await response.buffer();
-      await conn.sendFile(m.chat, imageBuffer, 'shizo.techie.error.png', msg, m);
+      await conn.sendFile(m.chat, imageBuffer, 'shizo.techie.error.png', msg, m, null, rpgc);
     } else {
       throw bug
     }
