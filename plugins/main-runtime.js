b1b9ad0f5486d286fbb7b4f3d0dc44a720ae9dd8
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-02-08 11:50:15 +0530

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -1,6 +1,16 @@
-
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-	
+//made with ❤️ by Shizo 
+import { cpus as _cpus, totalmem, freemem } from 'os'
+import util from 'util'
+import os from 'os'
+import { performance } from 'perf_hooks'
+import { sizeFormatter } from 'human-readable'
+let format = sizeFormatter({
+  std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
+  decimalPlaces: 2,
+  keepTrailingZeroes: false,
+  render: (literal, symbol) => `${literal} ${symbol}B`,
+})
+let handler = async (m, { conn, isRowner}) => {
 	let _muptime
     if (process.send) {
       process.send('uptime')
@@ -10,17 +20,59 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-   m.reply(`🎭 *Bot active duration*  \n\n${muptime}`) 
+  const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+  const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
+  const used = process.memoryUsage()
+  const cpus = _cpus().map(cpu => {
+    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+    return cpu
+  })
+  const cpu = cpus.reduce((last, cpu, _, { length }) => {
+    last.total += cpu.total
+    last.speed += cpu.speed / length
+    last.times.user += cpu.times.user
+    last.times.nice += cpu.times.nice
+    last.times.sys += cpu.times.sys
+    last.times.idle += cpu.times.idle
+    last.times.irq += cpu.times.irq
+    return last
+  }, {
+    speed: 0,
+    total: 0,
+    times: {
+      user: 0,
+      nice: 0,
+      sys: 0,
+      idle: 0,
+      irq: 0
+    }
+  })
+  let old = performance.now()
+
+  await conn.sendMessage(m.chat, {
+          react: {
+            text: `⏱️`,
+            key: m.key,
+          }})
+  let neww = performance.now()
+  let speed = neww - old
+let text = `${language['irt']()}
+${muptime}`
+await conn.sendMessage(m.chat, { text: text, mentions: [m.sender] }, { quoted: m })
+
 }
 handler.help = ['runtime']
-handler.tags = ['main']
-handler.command = ['runtime', 'uptime']
+handler.tags = ['info']
+handler.command = /^(uptime|runtime)$/i
 export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
 
 function clockString(ms) {
   let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, 'd ', h, 'h ', m, 'm ', s, 's '].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
 }
