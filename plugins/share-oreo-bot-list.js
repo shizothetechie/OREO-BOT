a70176125dd1072c5380148ab35f9be652c80335
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-02-11 21:34:05 +0530

    Delete plugins/share-oreo-bot-list.js

--- plugins/share-oreo-bot-list.js
+++
@@ -1,7 +0,0 @@
-async function handler(m, { usedPrefix }) {
-let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-await m.reply(users.map(v => 'wa.me/' + v.jid.replace(/[^0-9]/g, '') + ` (${v.name})`).join('\n'))}
-handler.command = ['sharedbots']
-handler.help = ['sharedbots']
-handler.tags = ['share-oreo']
-export default handler
