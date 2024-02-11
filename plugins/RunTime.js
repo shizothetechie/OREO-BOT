90fefeedecde6a8c547c1d8b85a0fdd340636bd2
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-02-11 07:21:01 +0530

    FIX RUNTIME.JS THROWING ERROR 🥵🔥

--- plugins/RunTime.js
+++ plugins/RunTime.js
@@ -4,22 +4,71 @@ import util from 'util'
 import os from 'os'
 import { performance } from 'perf_hooks'
 import { sizeFormatter } from 'human-readable'
+let format = sizeFormatter({
+  std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
+  decimalPlaces: 2,
+  keepTrailingZeroes: false,
+  render: (literal, symbol) => `${literal} ${symbol}B`,
+})
 let handler = async (m, { conn, isRowner}) => {
 	let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
     let muptime = clockString(_muptime)
+  const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+  const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
+  const used = process.memoryUsage()
+  const cpus = _cpus().map(cpu => {
+    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+    return cpu
+  })
+  const cpu = cpus.reduce((last, cpu, _, { length }) => {
+    last.total += cpu.total
+    last.speed += cpu.speed / length
+    last.times.user += cpu.times.user
+    last.times.nice += cpu.times.nice
+    last.times.sys += cpu.times.sys
+    last.times.idle += cpu.times.idle
+    last.times.irq += cpu.times.irq
+    return last
+  }, {
+    speed: 0,
+    total: 0,
+    times: {
+      user: 0,
+      nice: 0,
+      sys: 0,
+      idle: 0,
+      irq: 0
+    }
+  })
+  let old = performance.now()
+
   await conn.sendMessage(m.chat, {
           react: {
-            text: clock,
+            text: `⏱️`,
             key: m.key,
           }})
+  let neww = performance.now()
+  let speed = neww - old
 let text = `Bot is Running from 
-${muptime}`
+${muptime} \n\n🥵🔥 Support us https://github.com/shizothetechie/oreo-bot`
 await conn.sendMessage(m.chat, { text: text, mentions: [m.sender] }, { quoted: m })
+
 }
-handler.help = ['runtime', 'uptime']
-handler.tags = ['main']
+handler.help = ['runtime']
+handler.tags = ['info']
 handler.command = /^(uptime|runtime)$/i
 export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
 function clockString(ms) {
   let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
