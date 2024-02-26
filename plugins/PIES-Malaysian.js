45ab5b5be07d13df6542a649b49ae302b85d0e35
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-02-26 14:36:22 +0530

    Update PIES-Malaysian.js

--- plugins/PIES-Malaysian.js
+++ plugins/PIES-Malaysian.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
 
 let msg = `Malaysia Hot Girl 🥵🔥`
-let endpoint = `https://shizoapi.onrender.com/api/pies/malaysia?apikey=shizo${shizokeys}`
+let endpoint = `https://shizoapi.onrender.com/api/pies/malaysia?apikey=${shizokeys}`
 const response = await fetch(endpoint);
 if (response.ok) {
       const imageBuffer = await response.buffer();
