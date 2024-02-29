c0360e8ae6270d84d02091c85a652dccc392ca1d
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-02-29 17:25:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ import fs from 'fs'
 
 //OwnerShip
 global.owner = [
-  [process.env.OWNER_NUMBER || '919172389527', process.env.OWNER_NAME || 'Shizo Techie ❤️✨', true],
-  ['919172389527', 'Developer Shizo 🤖', true]
+  [process.env.OWNER_NUMBER || '919637987574', process.env.OWNER_NAME || 'Developer Shizo 🤖', true],
+  ['919637987574', 'Shizo Techie ❤️✨', true]
 ]
 global.mods = []
 global.prems = []
