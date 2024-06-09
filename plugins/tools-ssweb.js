5c610aeebd2960a72b29674e7391fe63dd3bfa2e
Xei-Sensei <xeisensei7@gmail.com>
2024-06-10 00:58:10 +0530

    Ssweb Fixed by Xei Sensei

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -1,10 +1,9 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
-if (!args[0]) return conn.reply(m.chat, '*[🔎] give url*', m)
+if (!args[0]) return conn.reply(m.chat, '*Give me the link dumbo 😂*', m)
 let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
-conn.sendFile(m.chat, ss, 'error.png', args[0], m)}
+conn.sendFile(m.chat, ss, 'xei.png', args[0], m)}
 handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
 handler.tags = ['tools']
 handler.command = /^ss(web)?f?$/i
 export default handler
-
