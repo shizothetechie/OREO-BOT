090c672109ef97263885df3cc3d7810a2f72bb27
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-06-22 16:22:43 +0530

    Create shizo_modules.js

---
+++ shizo_modules.js
@@ -0,0 +1,6 @@
+//Shizo_modules importing system 
+import GIFBufferToVideoBuffer from './shizo_modules/giftobuffer.js'
+
+
+//Shizo_modulea quick access system
+global.GIFBufferToVideoBuffer = GIFBufferToVideoBuffer
