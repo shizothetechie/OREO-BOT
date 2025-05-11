5bc5f7a67bd5cf13796704ab30d2cfc19c904238
Yugesh Singh <yugeshsingh27@gmail.com>
2025-05-11 07:44:49 +0530

    Delete oreoXmod.js

--- oreoXmod.js
+++
@@ -1,8 +0,0 @@
-//oreoXmod importing system 
-import GIFBufferToVideoBuffer from './oreoXmod/giftobuffer.js'
-import displayLoadingScreen from './oreoXmod/loading.js'
-
-
-//oreoXmod quick access system
-global.GIFBufferToVideoBuffer = GIFBufferToVideoBuffer
-global.displayLoadingScreen = displayLoadingScreen
